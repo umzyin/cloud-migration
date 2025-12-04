3a27c4d77c70140eb0feb3ade7f7657fee00cd37
unknown <umzy@UMZY.AD>
2025-12-04 19:14:01 +0000

    Initial commit - AzWinApp-Web Windows Auth + SQL POC

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Web.Mvc;
+using AzWinApp.Models;
+
+namespace AzWinApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            var userName = User?.Identity?.Name ?? "Unknown";
+
+            var repo = new EmployeeRepository();
+            var employees = repo.GetAll();
+
+            var vm = new HomeViewModel
+            {
+                CurrentUser = userName,
+                Employees = employees
+            };
+
+            return View(vm);
+        }
+    }
+
+    public class HomeViewModel
+    {
+        public string CurrentUser { get; set; }
+        public System.Collections.Generic.IEnumerable<Employee> Employees { get; set; }
+    }
+}
---
+++ Models/EmployeeRepository.cs.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+
+namespace AzWinApp.Models
+{
+    public class EmployeeRepository
+    {
+        private readonly string _connectionString;
+
+        public EmployeeRepository()
+        {
+            _connectionString = ConfigurationManager
+                .ConnectionStrings["DefaultConnection"]
+                .ConnectionString;
+        }
+
+        public IEnumerable<Employee> GetAll()
+        {
+            var employees = new List<Employee>();
+
+            using (var conn = new SqlConnection(_connectionString))
+            using (var cmd = new SqlCommand("SELECT Id, Name, Department FROM Employees", conn))
+            {
+                conn.Open();
+                using (var reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        employees.Add(new Employee
+                        {
+                            Id = reader.GetInt32(0),
+                            Name = reader.GetString(1),
+                            Department = reader.IsDBNull(2) ? null : reader.GetString(2)
+                        });
+                    }
+                }
+            }
+
+            return employees;
+        }
+    }
+}
